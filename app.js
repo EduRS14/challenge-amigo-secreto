c6edbe08eaaefe2f8795247d5e16a2f1f5947a06
EduRS14 <eduardo14.05rivas@gmail.com>
2025-02-01 10:52:51 -0500

    feature: Agregación de la función agregarAmigo para agregar el valor ingresado en el input a un arreglo

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let nombre = document.getElementById("amigo").value;
+    if (nombre == "") {
+        alert("Por favor, ingrese un nombre");
+    } else {
+        amigos.push(nombre);
+    }
+    document.getElementById("amigo").value = "";
+}
